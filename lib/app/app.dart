6b8904ed1d4266fc79a960c32f08fd43fd095f92
Adriel Bomfim <adrielvbomfim@gmail.com>
2025-10-24 23:13:06 -0300

    refactor: moves blocproviders to app class

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:very_good_coffee/app/injection_container.dart' as injection;
 import 'package:very_good_coffee/app/ui/app_colors.dart';
 import 'package:very_good_coffee/features/coffee/coffee.dart';
+import 'package:very_good_coffee/features/coffee/presentation/cubit/saved_images_cubit.dart';
 import 'package:very_good_coffee/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -20,7 +23,17 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const HomeScreen(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => injection.dependency<CoffeeCubit>(),
+          ),
+          BlocProvider(
+            create: (_) => injection.dependency<SavedImagesCubit>(),
+          ),
+        ],
+        child: const HomeScreen(),
+      ),
     );
   }
 }
--- lib/features/coffee/presentation/screens/home_screen.dart
+++ lib/features/coffee/presentation/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:very_good_coffee/app/injection_container.dart' as injection;
 import 'package:very_good_coffee/features/coffee/coffee.dart';
 import 'package:very_good_coffee/features/coffee/presentation/cubit/saved_images_cubit.dart';
 import 'package:very_good_coffee/features/coffee/presentation/screens/saved_images_screen.dart';
@@ -16,116 +15,104 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => injection.dependency<CoffeeCubit>(),
-        ),
-        BlocProvider(
-          create: (_) => injection.dependency<SavedImagesCubit>(),
-        ),
-      ],
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(l10n.homeAppBarTitle),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: BlocBuilder<CoffeeCubit, CoffeeState>(
-              builder: (context, coffeeState) {
-                if (coffeeState is CoffeeLoading) {
-                  return const CircularProgressIndicator();
-                } else if (coffeeState is CoffeeLoaded) {
-                  return SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Stack(
-                          alignment: Alignment.topRight,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.homeAppBarTitle),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                BlocBuilder<CoffeeCubit, CoffeeState>(
+                  builder: (context, coffeeState) {
+                    if (coffeeState is CoffeeLoaded) {
+                      return Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          Image(
+                            image: Image.memory(
+                              coffeeState.fetchedCoffeeImage.bytes,
+                            ).image,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child:
+                                BlocBuilder<SavedImagesCubit, SavedImagesState>(
+                                  builder: (context, savedImagesState) {
+                                    final isMarkedAsSaved =
+                                        savedImagesState is SavedImagesLoaded &&
+                                        savedImagesState.savedImages.contains(
+                                          coffeeState.fetchedCoffeeImage,
+                                        );
+
+                                    final isWidgetDisabled =
+                                        savedImagesState is SavedImagesError;
+
+                                    return SaveImageWidget(
+                                      coffeeImage:
+                                          coffeeState.fetchedCoffeeImage,
+                                      isMarkedAsSaved: isMarkedAsSaved,
+                                      isWidgetDisabled: isWidgetDisabled,
+                                    );
+                                  },
+                                ),
+                          ),
+                        ],
+                      );
+                    } else if (coffeeState is CoffeeError) {
+                      return InkWell(
+                        onTap: () async {
+                          await context.read<CoffeeCubit>().getNewCoffeeImage();
+                        },
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            Image(
-                              image: Image.memory(
-                                coffeeState.fetchedCoffeeImage.bytes,
-                              ).image,
+                            Icon(
+                              Icons.refresh,
+                              size: 48,
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child:
-                                  BlocBuilder<
-                                    SavedImagesCubit,
-                                    SavedImagesState
-                                  >(
-                                    builder: (context, savedImagesState) {
-                                      final isMarkedAsSaved =
-                                          savedImagesState
-                                              is SavedImagesLoaded &&
-                                          savedImagesState.savedImages.contains(
-                                            coffeeState.fetchedCoffeeImage,
-                                          );
-
-                                      final isWidgetDisabled =
-                                          savedImagesState is SavedImagesError;
-
-                                      return SaveImageWidget(
-                                        coffeeImage:
-                                            coffeeState.fetchedCoffeeImage,
-                                        isMarkedAsSaved: isMarkedAsSaved,
-                                        isWidgetDisabled: isWidgetDisabled,
-                                      );
-                                    },
-                                  ),
+                            SizedBox(height: 4),
+                            Text(
+                              'Try again',
+                              style: TextStyle(fontSize: 16),
                             ),
                           ],
+                        ),
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+                const SizedBox(height: 16),
+                Builder(
+                  builder: (context) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            unawaited(
+                              context.read<CoffeeCubit>().getNewCoffeeImage(),
+                            );
+                          },
+                          child: Text(l10n.newCoffeeButton),
                         ),
                         const SizedBox(height: 16),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: () {
-                                unawaited(
-                                  context
-                                      .read<CoffeeCubit>()
-                                      .getNewCoffeeImage(),
-                                );
-                              },
-                              child: Text(l10n.newCoffeeButton),
-                            ),
-                            const SizedBox(height: 16),
-                            TextButton(
-                              onPressed: () {
-                                unawaited(_goToSavedImagesScreen(context));
-                              },
-                              child: Text(l10n.favoriteCoffeesButton),
-                            ),
-                          ],
+                        TextButton(
+                          onPressed: () {
+                            unawaited(_goToSavedImagesScreen(context));
+                          },
+                          child: Text(l10n.favoriteCoffeesButton),
                         ),
                       ],
-                    ),
-                  );
-                } else {
-                  return InkWell(
-                    onTap: () async {
-                      await context.read<CoffeeCubit>().getNewCoffeeImage();
-                    },
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.refresh,
-                          size: 48,
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          'Try again',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-              },
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ),
