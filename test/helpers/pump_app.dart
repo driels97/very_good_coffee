670aec4660cc57c3203716662e52cb5dc977b390
Adriel Bomfim <adrielvbomfim@gmail.com>
2025-10-26 22:14:45 -0300

    feat: creates mock cubits and adds them into pumpApp method

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,14 +1,37 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:very_good_coffee/features/coffee/presentation/coffee_presentation.dart';
 import 'package:very_good_coffee/l10n/l10n.dart';
 
+class MockCoffeeCubit extends MockCubit<CoffeeState>
+    implements CoffeeCubit {}
+
+class MockSavedImagesCubit extends MockCubit<SavedImagesState>
+    implements SavedImagesCubit {}
+
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
+  Future<void> pumpApp({
+    required Widget widget,
+    CoffeeCubit? coffeeCubit,
+    SavedImagesCubit? savedImagesCubit,
+  }) {
     return pumpWidget(
-      MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: widget,
+      MultiBlocProvider(
+        providers: [
+          BlocProvider.value(
+            value: coffeeCubit ?? MockCoffeeCubit(),
+          ),
+          BlocProvider.value(
+            value: savedImagesCubit ?? MockSavedImagesCubit(),
+          ),
+        ],
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: widget,
+        ),
       ),
     );
   }
